26bb3c9934e69c80594a7fb3b6e071ee591b779d
Leonardo Torres Rodrigues <leonardo.t.rodri@gmail.com>
2026-02-18 20:53:25 -0300

    doc: semana 4: add documentação por meio de comentários e docstring

--- semana4/Exemplo 03/exemplo03.py
+++ semana4/Exemplo 03/exemplo03.py
@@ -1,6 +1,7 @@
 def f(x):
-    res = x ** 2 + 1
-    return res
+    'Exemplo de documentação de funções que será impressa por meio da função help()'
+    res = x ** 2 + 1  # calcula x ao quadrado + 1
+    return res       # retorna o resultado
 
 
 x = int(input('Passe o valor de x: '))
